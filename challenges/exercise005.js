5d68fa4a4923e9818706e00f0c8b45ebd46ed0b1
Sjlowman <sarah.j.lowman@gmail.com>
2023-05-21 13:51:11 +0100

    findNextNumber

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,36 +1,38 @@
 export const findNextNumber = (nums, n) => {
-	if (nums === undefined) throw new Error('nums is required');
-	if (n === undefined) throw new Error('n is required');
-	// Your code here!
+  if (nums === undefined) throw new Error("nums is required");
+  if (n === undefined) throw new Error("n is required");
+  const indexOfNumber = nums.findIndex((x) => x === n);
+  if (indexOfNumber === -1) return null;
+  return nums[indexOfNumber + 1] || null;
 };
 
 export const count1sand0s = (str) => {
-	if (str === undefined) throw new Error('str is required');
-	// Your code here!
+  if (str === undefined) throw new Error("str is required");
+  // Your code here!
 };
 
 export const reverseNumber = (n) => {
-	if (n === undefined) throw new Error('n is required');
-	// Your code here!
+  if (n === undefined) throw new Error("n is required");
+  // Your code here!
 };
 
 export const sumArrays = (arrs) => {
-	if (arrs === undefined) throw new Error('arrs is required');
-	// Your code here!
+  if (arrs === undefined) throw new Error("arrs is required");
+  // Your code here!
 };
 
 export const arrShift = (arr) => {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+  if (arr === undefined) throw new Error("arr is required");
+  // Your code here!
 };
 
 export const findNeedle = (haystack, searchTerm) => {
-	if (haystack === undefined) throw new Error('haystack is required');
-	if (searchTerm === undefined) throw new Error('searchTerm is required');
-	// Your code here!
+  if (haystack === undefined) throw new Error("haystack is required");
+  if (searchTerm === undefined) throw new Error("searchTerm is required");
+  // Your code here!
 };
 
 export const getWordFrequencies = (str) => {
-	if (str === undefined) throw new Error('str is required');
-	// Your code here!
+  if (str === undefined) throw new Error("str is required");
+  // Your code here!
 };
